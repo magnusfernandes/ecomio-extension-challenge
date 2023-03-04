b33fbe0099e5bdf2437e9351d97c7cdb164826fa
Magnus Fernandes <magnusfernandes1295@hotmail.com>
2023-03-05 04:14:39 +0530

    DomElement.ts code review

--- src/DOMElement.ts
+++ src/DOMElement.ts
@@ -1,5 +1,11 @@
+/**
+ * @todo use accessibility modifier eg: public constructor(){}
+ */
 export default class DOMElement {
   parent: HTMLElement;
+  /**
+   * @todo make use of specific type
+   */
   styles: string | string[];
   element: HTMLElement;
 
@@ -7,21 +13,34 @@ export default class DOMElement {
     parent: HTMLElement,
     styles: string | string[],
     text?: Promise<string | string[]> | string | string[],
+    /**
+     * @todo unused param
+     */
     link?: string,
   ) {
     this.parent = parent;
     this.styles = styles;
+    /**
+     * @todo property assigned twice
+     */
     this.element = document.createElement('div');
     this.element = this.addStyles(this.styles);
     if (text) this.addText(text);
   }
-
+  /**
+   @todo Make the method flexible to append multiple child,
+   example present as addElementsToDom() method below
+   @todo uneccesary return element, this.element will get manipulated 
+  */
   appendToDom(): HTMLElement {
     const elementAlreadyAppended = this.parent.querySelector(`.${this.styles}`) !== null;
     if (!elementAlreadyAppended) this.parent.appendChild(this.element);
     return this.element;
   }
 
+  /**
+   *   @todo uneccesary return element, this.element will get manipulated
+   */
   prependToDom(overwrite?: boolean): HTMLElement {
     const existedElement = this.parent.querySelector(`:scope > .${this.styles}`);
     if (existedElement) {
@@ -32,11 +51,17 @@ export default class DOMElement {
     return this.element;
   }
 
+  /**
+   * @todo uneccesary return element, this.element will get manipulated
+   */
   addStyles(styles: string | string[]): HTMLElement {
     this.style(this.element, styles);
     return this.element;
   }
 
+  /**
+   * @todo use an interface for params
+   */
   private async addText(text: Promise<string | string[]> | string | string[]) {
     if (text instanceof Promise) text = await text;
     if (typeof text === 'string') {
@@ -53,7 +78,9 @@ export default class DOMElement {
     textElem.appendChild(textNode);
     this.element.appendChild(textElem);
   }
-
+  /**
+   * @todo uneccesary return element, this.element will get manipulated
+   */
   private style(elem: HTMLElement, styles: string | string[]): HTMLElement {
     if (typeof styles === 'string') {
       elem.classList.add(styles);
@@ -65,7 +92,9 @@ export default class DOMElement {
     return elem;
   }
 
-  // custom methods
+  /**
+   appends list of  children to the parent element
+  */
   addElementsToDom(elements: HTMLElement[]) {
     const elementAlreadyAppended = this.parent.querySelector(`.${this.styles}`) !== null;
     if (!elementAlreadyAppended) this.parent.appendChild(this.element);
